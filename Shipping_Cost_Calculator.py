8c6609509339fdce307858988903713001ac3861
nehhub2024 <missneh30m@gmail.com>
2026-07-06 23:32:15 +0530

    Add comments for clarity in Shipping Cost Calculator

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,7 @@
 # Here is a new update by nehhub2024
 # Shipping Cost Calculator
 
-
+ 
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
